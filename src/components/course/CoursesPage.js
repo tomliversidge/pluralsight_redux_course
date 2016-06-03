5f9f00022ff096b81f24600d10b7683187656b34
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-06-03 10:50:20 +0100

    add courses page

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,18 @@
+import React, {PropTypes} from 'react';
+
+class CoursesPage extends React.Component {
+  render() {
+    return (
+      <div >
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+CoursesPage.propTypes = {};
+CoursesPage.defaultProps = {};
+
+export default CoursesPage;
+
+
