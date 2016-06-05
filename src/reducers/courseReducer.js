dd7683f4027ec9b49608077fef16efa6ca23631c
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-06-05 08:41:04 +0100

    use constants not magic strings

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -1,6 +1,8 @@
+import * as types from '../actions/actionTypes';
+
 export default function courseReducer(state = [], action){
   switch(action.type){
-    case 'CREATE_COURSE':
+    case types.CREATE_COURSE:
         return [
           ...state,
           Object.assign({}, action.course) //copy action.course so as not to reference/mutate what is passed in
