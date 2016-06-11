9a77c897eee47398a392c0f2f141f3c649d715d8
Tom Liversidge <tliversidge@roishoppermarketing.com>
2016-06-11 14:54:19 +0100

    add course title validation

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -5,7 +5,7 @@ import * as courseActions from './courseActions';
 import CourseForm from './CourseForm';
 import toastr from 'toastr';
 
-class ManageCoursePage extends React.Component {
+export class ManageCoursePage extends React.Component {
   constructor(props, context){
     super(props, context);
 
@@ -25,8 +25,21 @@ class ManageCoursePage extends React.Component {
     return this.setState({course:course});
   }
 
+  courseFormIsValid(){
+    let formIsValid = true;
+    let errors = {};
+    if (this.state.course.title.length < 5) {
+      errors.title = 'Title must be at least 5 characters.';
+      formIsValid = false;
+    }
+    this.setState({errors: errors});
+    return formIsValid;
+  }
   saveCourse(event){
     event.preventDefault();
+    if (!this.courseFormIsValid()){
+      return;
+    }
     this.setState({saving: true});
     this.props.actions.saveCourse(this.state.course)
       .then(()=> this.redirect())
@@ -108,5 +121,3 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
-
-
---
+++ src/components/course/ManageCoursePage.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import expect from 'expect';
+import {ManageCoursePage} from './ManageCoursePage';
+
+function setup() {
+  let props = {
+    course: {id: '', watchHref: '', title: '', authorId: '', length: '', category: ''},
+    authors: [],
+    actions: {
+      saveCourse: () => { return Promise.resolve();}
+    }
+  };
+  return mount(<ManageCoursePage {...props}/>);
+}
+
+describe('Given a Manage Course Page', () => {
+
+  it('sets an error message when trying to save with a title less than 5 characters long', () => {
+    const wrapper = setup();//
+    const saveButton = wrapper.find('input').last();
+    expect(saveButton.prop('type')).toBe('submit');
+    saveButton.simulate('click');
+    expect(wrapper.state().errors.title).toBe('Title must be at least 5 characters.');
+  });
+
+});
